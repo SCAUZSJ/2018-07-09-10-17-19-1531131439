1bf11aa0f9626b9234842f01cb8f9d7f3036fa1f
SCAUZSJ <895104160@qq.com>
2019-07-11 23:00:48 +0800

    implement practice07

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,28 @@
 package practice07;
 
-public class Student {
+public class Student extends  Person{
+    private Klass klass;
+
+    public Student(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+
+    @Override
+    public String introduce() {
+        return super.introduce()+" I am a Student. I am at Class "+this.klass.getNumber()+".";
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,39 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+    final String info=" I am a Teacher. ";
+
+    public Teacher(){}
+    public Teacher(Klass klass) {
+        this.klass = klass;
+    }
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+    @Override
+    public String introduce() {
+        if(this.klass != null){
+            return super.introduce()+this.info+"I teach Class "+this.klass.getNumber()+".";
+        }
+        return super.introduce()+this.info+"I teach No Class.";
+    }
+    public String introduceWith(Student student){
+        if(student.getKlass().getNumber() ==this.klass.getNumber()){
+            return super.introduce()+this.info+"I teach "+student.getName()+".";
+        }
+        return super.introduce()+this.info+"I don't teach "+student.getName()+".";
+    }
 }
